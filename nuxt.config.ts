5fba0b6919455becd60e3dc966dd4207b7970030
Mohamedahmed27111 <mohamedahmed27169@gmail.com>
2024-08-11 13:45:05 +0300

    SEO 2

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr:false,
+  app: {
+    head: {
+      title: 'Serenade Sphere',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'find your artist best music' },
+        { name: 'keywords', content: 'The Weeknd, kanye west, Future , sza , Lana Del Rey ' },
+        { name: 'author', content: 'MR willson' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ],
+      script: [
+        // You can include global scripts here
+      ]
+    }
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   css:['~/assets/styles/main.scss'],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
  
-  app: {
-    head: {
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { name: 'description', content: 'find your artist best music' },
-        { name: 'keywords', content: 'The Weeknd, kanye west, Future , sza , Lana Del Rey ' },
-        { name: 'author', content: 'MR willson' }
-      ],
-      link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-      ],
-      script: [
-        // You can include global scripts here
-      ]
-    }
-  },
+
   content: [
     "./node_modules/flowbite/**/*.js"
 ],
